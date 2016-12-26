1b39e322c10f8c70d2145417ed27d7871b3c6a57
Charles Goh <charles.goh@live.com>
2016-12-26 16:34:41 +0800

    New Logo
    Took an icon from google images as practice. Will be changing the icon
    soon

--- src/ippttracker/MainApp.java
+++ src/ippttracker/MainApp.java
@@ -11,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -51,6 +52,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("IPPT Tracker");
+        
+        // Set the application icon
+        this.primaryStage.getIcons().add(new Image("file:resources/icons/testlogo.png"));
         
         initRootLayout();
         
@@ -112,6 +116,9 @@ public class MainApp extends Application {
             dialogStage.initOwner(primaryStage);
             Scene scene = new Scene(page);
             dialogStage.setScene(scene);
+            
+            //Set logo of dialog Stage
+            dialogStage.getIcons().add(new Image("file:resources/icons/testlogo.png"));
             
             //Set the person into the controller
             ResultEditDialogController controller = loader.getController();
